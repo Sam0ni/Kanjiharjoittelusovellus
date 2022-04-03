592eca0770f86e50bd99ca8dbb92e38ddaf629b8
Samppa <samppasalo16@gmail.com>
2022-04-03 15:39:23 +0300

    .

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import re
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
+uri = os.getenv("postgres://otwsvshvogmloe:40492ca5b91ecfc849a2f417357b17d49d212d8eb33fea51af062c517da95535@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/ddf3jlnb13tvtq")  # or other relevant config var
 if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+    uri = uri.replace("postgres://", "postgresql://")
 
 app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
