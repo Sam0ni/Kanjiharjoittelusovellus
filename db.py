f38ddebfe0d8d33c23bdd5ac9ae7d818fcbb8eb6
Samppa <samppasalo16@gmail.com>
2022-04-23 17:55:11 +0300

    change sqlalchemy requirements

--- db.py
+++ db.py
@@ -1,7 +1,12 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+import re
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+uri = getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
