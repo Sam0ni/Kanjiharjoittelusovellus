dd66c1a0357d684afe93fbe596bf2ac658f4dc58
Samppa Salo <samppasalo16@gmail.com>
2023-08-04 17:08:15 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,9 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 import re
 
-uri = getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 db = SQLAlchemy(app)
